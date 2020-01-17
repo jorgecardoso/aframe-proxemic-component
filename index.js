cee2666b039c22f500da1a2824da7ecda8f5f9b0
Jorge C. S. Cardoso <jorgecardoso@ieee.org>
2020-01-17 15:59:10 +0000

    updated version number

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 if (typeof AFRAME === 'undefined') {
   throw new Error('Component attempted to register before AFRAME was available.');
 }
-console.log('A-Frame Proxemic Components Version: 0.0.12 (Date 2020-01-17, Commit #47a3413)');
+console.log('A-Frame Proxemic Components Version: 0.0.13 (Date 2020-01-17, Commit #00a58bf)');
 
 require("./src/proximity-sensor.js")
 require( "./src/compass-sensor.js")
